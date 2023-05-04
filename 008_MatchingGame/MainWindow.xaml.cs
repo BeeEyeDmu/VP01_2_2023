e53e9fb101e57ae748b9b105e7effa7eab397b14
BeeEye Dmu <bikang@konyang.ac.kr>
2023-05-04 15:11:46 +0900

    Add files via upload

--- 008_MatchingGame/MainWindow.xaml.cs
+++ 008_MatchingGame/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading; // DispatcherTimer
 
 namespace _008_MatchingGame
 {
@@ -20,14 +21,35 @@ namespace _008_MatchingGame
   /// </summary>
   public partial class MainWindow : Window
   {
-    Random r = new Random();
-    string[] icon = { "딸기", "레몬", "모과", "배", "사과", "수박", "파인애플", "포도" };
-    int[] rnd = new int[16];  // 0으로 초기화된다
+    private Random r = new Random();
+    private string[] icon = { "딸기", "레몬", "모과", "배", "사과", "수박", "파인애플", "포도" };
+    private int[] rnd = new int[16];  // 0으로 초기화된다
+    private Button first = null;
+    private Button second = null;
+
+    // 타이머 사용시 주의점
+    // WinForm: Timer t = new Timer()
+    // WPF: DispatcherTimer t = new DispatcherTimer()
+    private DispatcherTimer myTimer = new DispatcherTimer();
+    private int matched = 0;    // 맞춘 카드의 숫자
 
     public MainWindow()
     {
       InitializeComponent();
       BoardSet();
+
+      // 타이머 처리
+      myTimer.Interval = new TimeSpan(0,0,0,0,750); // 0.75초
+      myTimer.Tick += MyTimer_Tick;
+    }
+
+    private void MyTimer_Tick(object sender, EventArgs e)
+    {
+      myTimer.Stop();
+      first.Content = MakeImage("../../Images/check.png");
+      second.Content = MakeImage("../../Images/check.png");
+      first = null;
+      second = null;
     }
 
     private void BoardSet()
@@ -51,8 +73,60 @@ namespace _008_MatchingGame
       Button btn = sender as Button;
 
       string fruit = "../../Images/" + icon[(int)btn.Tag];
-      btn.Content = MakeImage(fruit + ".png");
+
+      if (first == null) // 지금 눌린 버튼이 첫번째 버튼
+      {
+        first = btn;
+        btn.Content = MakeImage(fruit + ".png");
+        return;
+      }
+      else if (second == null)
+      {
+        second = btn;
+        btn.Content = MakeImage(fruit + ".png");
+      }
+      else
+        return;
+
+      // first와 second가 매칭되는지를 체크(Tag로 체크)
+      if((int)first.Tag == (int)second.Tag)
+      {
+        first = null;
+        second = null;
+        matched += 2;
+        if(matched >= 16) // 모두 다 맞추었으면
+        {
+          MessageBoxResult result = MessageBox.Show("성공! 다시 할까요?", 
+            "Success!", MessageBoxButton.YesNo);
+          if (result == MessageBoxResult.Yes) // 처음부터 다시 시작
+          {
+            // 1. rnd[] 초기화
+            ResetRnd();
+            // 2. board 초기화(board 안에 있는 Children을 삭제)
+            board.Children.Clear();
+            // 3. BoardSet()
+            BoardSet();
+            // 4. matched = 0 초기화
+            matched = 0;
+          }
+          else
+            this.Close();
+        }
+      }
+      else
+      {
+        myTimer.Start();
+      }
+
     }
+
+    // rnd[] 배열을 초기화한다
+    private void ResetRnd()
+    {
+      for(int i=0; i < 16; i++)
+        rnd[i] = 0;
+    }
+
 
     // v로 넘어온 이미지 파일을 img 객체로 만들어서 리턴하는 메소드
     private Image MakeImage(string v)
