25d42fe592889e23d7611ebf906171a4882318c7
BeeEye Dmu <bikang@konyang.ac.kr>
2023-05-25 15:55:30 +0900

    Add files via upload

---
+++ 011_PageNavigation/start.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _011_PageNavigation
+{
+  /// <summary>
+  /// start.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class start : Page
+  {
+    public start()
+    {
+      InitializeComponent();
+    }
+
+    private void btnInfo_Click(object sender, RoutedEventArgs e)
+    {
+      NavigationService.Navigate(new Uri("/Information.Xaml", UriKind.Relative));
+    }
+
+    private void btnLogin_Click(object sender, RoutedEventArgs e)
+    {
+      NavigationService.Navigate(new Uri("/Login.Xaml", UriKind.Relative));
+    }
+
+    private void btnSignUp_Click(object sender, RoutedEventArgs e)
+    {
+      NavigationService.Navigate(new Uri("/SignUp.Xaml", UriKind.Relative));
+    }
+  }
+}
---
+++ 012_SnakeBite/Game.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _012_SnakeBite
+{
+  /// <summary>
+  /// Game.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class Game : Window
+  {
+    Ellipse[] snakes = new Ellipse[30];
+    Ellipse egg;
+    Random r = new Random();
+    int visibleCount = 5; // 뱀 중에서 보이는 Ellipse의 갯수
+    int size = 10;        // Ellipse의 크기
+    public Game()
+    {
+      InitializeComponent();
+      InitEgg();
+    }
+
+    private void InitEgg()
+    {
+      egg = new Ellipse();
+      egg.Width = size;
+      egg.Height = size;
+      egg.Stroke = Brushes.Black;
+      egg.Fill = Brushes.Red;
+
+      // 캔버스 크기 Width="420" Height="320"
+      int x = r.Next(0, (int)field.Width / size);
+      int y = r.Next(0, (int)field.Height / size);
+
+      Point p = new Point(x*size, y*size);
+      egg.Tag = p;
+
+      field.Children.Add(egg);
+      Canvas.SetLeft(egg, x * size);
+      Canvas.SetTop(egg, y * size);
+    }
+  }
+}
